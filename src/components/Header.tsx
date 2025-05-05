f682424d6cc8dee5d49123438e86aa1d1b5c93dd
Royce Joe <roylory@gmail.com>
2025-05-05 12:43:58 -0700

    add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="border-b border-gray-300 py-4">
+      <h1 className="container mx-auto text-2xl font-bold text-center">Todo List</h1>
+    </header>
+  );
+};
+
+export default Header;
