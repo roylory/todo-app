ffbd2f967d84c7a4bd42c313d51a2164bd8743bd
Royce Joe <roylory@gmail.com>
2025-05-05 15:50:22 -0700

    slightly improve mobile style

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -21,12 +21,9 @@ const TodoItem: React.FC<TodoItemProps> = ({ id, description, isComplete, dueDat
         onChange={() => update(id, !isComplete)}
         className="w-5 h-5 accent-gray-500 focus:ring-gray-500 cursor-pointer"
       />
-      <span className={twMerge(
-        'text-base flex-1 text-gray-800',
-        isComplete && 'line-through')}
-      >{description}</span>
+      <span className={twMerge('text-base flex-1 text-gray-800 line-clamp-1', isComplete && 'line-through')}>{description}</span>
       {dueDate && (
-        <span className="text-gray-500 text-sm font-mono">
+        <span className="text-gray-500 text-xs sm:text-sm font-mono">
           Due {new Date(dueDate).toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' })}
         </span>
       )}
